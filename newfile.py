4b53244f1619e9a997d09fd7e65ae7c4da4a3c09
ma.palacios.2017 <ma.palacios.2017@alumnos.urjc.es>
2018-03-16 16:16:26 +0100

    Initial upload of newfile.py

---
+++ newfile.py
@@ -0,0 +1,14 @@
+a = 1
+b = 0
+
+
+print("hello from PyCharm 0")
+print("hello from PyCharm 1")
+print("hello from PyCharm 2")
+
+
+print("hello from PyCharm 3")
+print("hello from PyCharm 4")
+print("hello from PyCharm 5")
+print("hello from PyCharm 6")
+print("hello from PyCharm 7")
