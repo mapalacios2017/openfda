ab714e576946034f8eb5d807f18e7883dbfb7fb1
mapalacios2017 <candpalaangemigu1999>
2018-03-20 16:30:48 +0100

    http-protocol

--- http protocol/server.py
+++ http protocol/server.py
@@ -1,10 +1,7 @@
 import http.server
 import socketserver
 
-# -- IP and the port of the server
-IP = "localhost"  # Localhost means "I": your local machine
-PORT = 8002
-
+PORT = 8000
 
 # HTTPRequestHandler class
 class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
@@ -14,21 +11,19 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         self.send_response(200)
 
         # Send headers
-        self.send_header('Content-type', 'text/html')
+        self.send_header('Content-type','text/html')
         self.end_headers()
 
         # Send message back to client
         message = "Hello world! " + self.path
         # Write content as utf-8 data
         self.wfile.write(bytes(message, "utf8"))
-        print("File served!")
         return
 
-
-# Handler = http.server.SimpleHTTPRequestHandler
+Handler = http.server.SimpleHTTPRequestHandler
 Handler = testHTTPRequestHandler
 
-httpd = socketserver.TCPServer((IP, PORT), Handler)
+httpd = socketserver.TCPServer(("", PORT), Handler)
 print("serving at port", PORT)
 httpd.serve_forever()
 
