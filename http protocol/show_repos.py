f468ab65cd2cb4a6b397e887d4eb80490deecdf9
mapalacios2017 <candpalaangemigu1999>
2018-03-20 16:10:09 +0100

    http-protocol

---
+++ http protocol/show_repos.py
@@ -0,0 +1,18 @@
+import http.client
+import json
+
+headers = {'User-Agent': 'http-client'}
+
+conn = http.client.HTTPSConnection("api.github.com")
+conn.request("GET", "/users/mapalacios2017/repos", None, headers)
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+repos_raw = r1.read().decode("utf-8")
+conn.close()
+
+repos = json.loads(repos_raw)
+
+repo = repos[0]
+print("The owner of the first repository is", repo['owner']['login'])
+print("The number of repos is", len(repos))
+print("the name(s) of the repo(s) is", repos['name'])
