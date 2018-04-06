6ec99abfc191c5df394e9b45fe1724cac4e7ae14
mapalacios2017 <ma.palacios.2017@alumnos.urjc.es>
2018-04-06 18:20:28 +0200

    4

--- openfda-4/server_openfda-4.py
+++ openfda-4/server_openfda-4.py
@@ -1,18 +1,17 @@
 import http.server
 import socketserver
+import json
+
+socketserver.TCPServer.allow_reuse_address = True
 
 PORT = 8005
 
-# HTTPRequestHandler class
 class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
-    # GET
     def do_GET(self):
 
-        # Send response status code
         self.send_response(200)
 
-        # Send headers
-        self.send_header('Content-type','text/html')
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
         if self.path == "/":
             with open("search.html") as f:
@@ -22,11 +21,17 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         elif "search" in self.path:
             params = self.path.split("?")[1]
             drug = params.split("&")[0].split("=")[1]
-            limit = params.split("&")[0].split("=")[1]
-            self.wfile.write(bytes(drug + " " + limit, "utf8"))
+            self.wfile.write(bytes(drug, "utf8"))
 
+            conn = http.client.HTTPSConnection("api.fda.gov")
+            conn.request("GET", '/drug/label.json?search=generic_name:' + self.wfile.write(bytes(drug, "utf8")), None, headers)
+            r1 = conn.getresponse()
+            print(r1.status, r1.reason)
+            repos_raw = r1.read().decode("utf-8")
+            conn.close()
 
         return
+
 
 Handler = http.server.SimpleHTTPRequestHandler
 Handler = testHTTPRequestHandler
