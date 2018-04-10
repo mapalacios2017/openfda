f6affd8cb462cb54217b32b787504d0e04a2b573
mapalacios2017 <ma.palacios.2017@alumnos.urjc.es>
2018-04-10 16:30:04 +0200

    open fda 4

--- openfda-4/server_openfda-4.py
+++ openfda-4/server_openfda-4.py
@@ -22,23 +22,20 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         elif "search" in self.path:
             params = self.path.split("?")[1]
             drug = params.split("&")[0].split("=")[1]
-            self.wfile.write(drug, "utf8")
 
-            if self.wfile.write(drug, "utf8") in self.path:
-                drugs = self.wfile.write(drug, "utf8")
+            headers = {'User-Agent': 'http-client'}
 
-                headers = {'User-Agent': 'http-client'}
+            conn = http.client.HTTPSConnection("api.fda.gov")
+            conn.request("GET", '/drug/label.json?search=generic_name:' + drug, None, headers)
+            r1 = conn.getresponse()
+            print(r1.status, r1.reason)
+            repos_raw = r1.read().decode("utf-8")
+            conn.close()
 
-                conn = http.client.HTTPSConnection("api.fda.gov")
-                conn.request("GET", '/drug/label.json?search=generic_name:' + drugs, None, headers)
-                r1 = conn.getresponse()
-                print(r1.status, r1.reason)
-                repos_raw = r1.read().decode("utf-8")
-                conn.close()
-
-                repos = json.loads(repos_raw)
-
-                repo = repos['results']
+            drugs = json.loads(repos_raw)
+            drugs = drugs['results']
+            drugs_id = "<ol>" + drugs[0]['id'] + "</ol>"
+            self.wfile.write(bytes(drugs_id, "utf8"))
 
         return
 
