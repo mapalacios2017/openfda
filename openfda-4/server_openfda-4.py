c74f54494fb3592982df5e45345a58acdb88d3e6
mapalacios2017 <ma.palacios.2017@alumnos.urjc.es>
2018-04-10 16:58:34 +0200

    open fda 4

--- openfda-4/server_openfda-4.py
+++ openfda-4/server_openfda-4.py
@@ -22,19 +22,23 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         elif "search" in self.path:
             params = self.path.split("?")[1]
             drug = params.split("&")[0].split("=")[1]
-
+            limit = params.split("&")[1].split("=")[1]
             headers = {'User-Agent': 'http-client'}
 
             conn = http.client.HTTPSConnection("api.fda.gov")
-            conn.request("GET", '/drug/label.json?search=generic_name:' + drug, None, headers)
+            conn.request("GET", '/drug/label.json?search=generic_name:' + drug + "&limit=" + limit, None, headers)
             r1 = conn.getresponse()
             print(r1.status, r1.reason)
             repos_raw = r1.read().decode("utf-8")
             conn.close()
 
             drugs = json.loads(repos_raw)
-            drugs = drugs['results']
-            drugs_id = "<ol>" + drugs[0]['id'] + "</ol>"
+            drug = drugs['results']
+
+            for drug in drugs['results']:
+                drugs_id = "<ol>" + drug['id'] + "</ol>"
+
+
             self.wfile.write(bytes(drugs_id, "utf8"))
 
         return
