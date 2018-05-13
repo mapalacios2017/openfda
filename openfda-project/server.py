9cdc769fba53453627f6ef9dcafe6777544d2ce3
ma.palacios.2017 <mapalacios>
2018-05-13 20:39:18 +0200

    final practice

--- openfda-project/server.py
+++ openfda-project/server.py
@@ -23,21 +23,21 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         elif "searchDrug" in self.path:
             params = self.path.split("?")[1]
             drug = params.split("&")[0].split("=")[1]
-            limit = params.split("&")[1].split("=")[1]
+            #limit = params.split("&")[1].split("=")[1]
             headers = {'User-Agent': 'http-client'}
 
             conn = http.client.HTTPSConnection("api.fda.gov")
-            conn.request("GET", '/drug/label.json?search=active_ingredient:' + drug + "&limit=" + limit, None, headers)
+            conn.request("GET", '/drug/label.json?search=active_ingredient:' + drug + "&limit=10", None, headers)
             r1 = conn.getresponse()
             print(r1.status, r1.reason)
             repos_raw = r1.read().decode("utf-8")
             conn.close()
 
             drugs = json.loads(repos_raw)
-            total_drugs = ""
+            total_drugs = "<ol>"
             for drug in drugs['results']:
-                drugs_info = "<ol>" + "Drug Id: " + drug['id'] + "</ol>"
-                total_drugs = total_drugs + drugs_info
+                total_drugs += "<li>" + "Drug Id: " + drug['id']
+                total_drugs += "</li>"
 
             self.wfile.write(bytes(total_drugs, "utf8"))
 
@@ -45,11 +45,11 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         elif "searchCompany" in self.path:
             params = self.path.split("?")[1]
             company = params.split("&")[0].split("=")[1]
-            limit = params.split("&")[1].split("=")[1]
+            #limit = params.split("&")[1].split("=")[1]
             headers = {'User-Agent': 'http-client'}
 
             conn = http.client.HTTPSConnection("api.fda.gov")
-            conn.request("GET", '/drug/label.json?search=openfda.manufacturer_name:' + company + "&limit=" + limit, None, headers)
+            conn.request("GET", '/drug/label.json?search=openfda.manufacturer_name:' + company + "&limit=10", None, headers)
             r1 = conn.getresponse()
             print(r1.status, r1.reason)
             repos_raw = r1.read().decode("utf-8")
